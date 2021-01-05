c49ea2182688637c5037b4f5ec586c7c08c8957b
tuanna <tuannguyenanh1412@gmail.com>
2021-01-06 01:58:39 +0700

    add socket io

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URI, (err) => {
   console.log('MongoDB connected');
 });
 
+// khởi tạo phần http cho server
 app.use(cors())
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -37,8 +38,42 @@ app.get('/', (req, res) => {
   res.send('pong');
 })
 
+// khởi tạo thêm phần websocket cho server
+const server = require('http').createServer(app);
+const options = {
+  cors: {
+    origin: '*'
+  }
+};
+
+const io = require('socket.io')(server, options);
+global.io = io;
+
+io.on('connection', socket => {
+  console.log(`${socket.id} connected`);
+
+  socket.on('join-room-post', (data) => {
+    socket.join(`post_${data.postId}`);
+  })
+
+  socket.on('someone-typing', (data) => {
+    const room = `post_${data.postId}`
+    socket.to(room).emit('user-typing', { user: data.user });
+  })
+
+  socket.on('new-comment', (data) => {
+    const room = `post_${data.postId}`
+    // bắn cho tất cả trong room ngoài trừ thằng socket đang .on
+    socket.to(room).emit('new-comment-added', data);
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.id} disconnect`);
+  });
+});
+
 // khởi tạo server
-app.listen(process.env.PORT || 8080, (err) => {
+server.listen(process.env.PORT || 8080, (err) => {
   if (err) throw err;
   console.log('Server started');
 });
